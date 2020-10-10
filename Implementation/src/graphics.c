eb6e4be23a6cf216f3824f09cb6c691465b3fe09
divyasappa21 <69084086+divyasappa21@users.noreply.github.com>
2020-10-10 11:02:35 +0530

    Create graphics.c

---
+++ Implementation/src/graphics.c
@@ -0,0 +1,12 @@
+#include<graphics.h>
+#include <stdio.h>
+int main()
+{
+
+ void circle( int x, int y, int radius );
+ int getdisplaycolor( int color );
+ int converttorgb( int color );
+ void delay( int msec );
+ int getch( );
+ int kbhit( );
+}
